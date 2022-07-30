242815bc20ebcb6b1432230d7574c5ac646203a1
KateVasilevskaya <7091764@gmail.com>
2022-07-30 15:19:55 +0200

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let elem = str.split("");
+  let stack = [];
+  for (i = 0; i < elem.length; i++) {
+    for (j = 0; j < bracketsConfig.length; j++) {
+      if (
+        elem[i] == bracketsConfig[j][1] &&
+        stack.length &&
+        stack[stack.length - 1] == bracketsConfig[j][0]
+      ) {
+        stack.pop();
+        break;
+      } else if (elem[i] == bracketsConfig[j][0]) {
+        stack.push(elem[i]);
+        break;
+      } else if (elem[i] == bracketsConfig[j][1]) {
+        return false;
+      }
+    }
+  }
+  return !stack.length;
+};
